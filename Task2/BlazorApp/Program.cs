1491c30df6068de171e233c2dc332de7adc21fb9
Aleksander <aciualeksander@gmail.com>
2024-05-08 18:05:33 +0200

    Added blazor app

---
+++ Task2/BlazorApp/Program.cs
@@ -0,0 +1,46 @@
+using BlazorApp.Components;
+using Microsoft.AspNetCore.Authentication.Certificate;
+
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+
+            builder.Services.AddAuthentication(
+                CertificateAuthenticationDefaults.AuthenticationScheme
+            ).AddCertificate();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
